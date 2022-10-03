5af46adebf247f59fd984ac44142a87c0dadc33a
Vinyks <kevinbryks@online.de>
2022-10-03 17:14:28 +0200

    Added Dialogue

--- StardewMotif/StardewMotif.cs
+++ StardewMotif/StardewMotif.cs
@@ -27,13 +27,25 @@ namespace StardewMotif
         }
         public void OnAssetRequested(object sender, AssetRequestedEventArgs e)
         {
-            ReplaceAsset<Texture2D>(e, "Portraits/Abigal", "Portraits/luny.png");
+            ReplaceAsset<Texture2D>(e, "Portraits/Abigail", "Portraits/luny.png"); 
+            ReplaceAsset<Texture2D>(e, "Characters/Abigail", "Characters/luny.png");
+            ReplaceAsset<Texture2D>(e, "Portraits/Abigail_Beach", "Portraits/luny_beach.png");
+            ReplaceAsset<Texture2D>(e, "Characters/Abigail_Beach", "Characters/luny_beach.png");
+
             ReplaceAsset<Texture2D>(e, "Portraits/Leah", "Portraits/mona.png");
-            ReplaceAsset<Texture2D>(e, "Portraits/Sam", "Portraits/fin.png");
+            ReplaceAsset<Texture2D>(e, "Characters/Leah", "Characters/mona.png");
+            ReplaceAsset<Texture2D>(e, "Portraits/Leah_Beach", "Portraits/mona_beach.png");
+            ReplaceAsset<Texture2D>(e, "Characters/Leah_Beach", "Characters/mona_beach.png");
+
+            ReplaceAsset<Texture2D>(e, "Portraits/Sam", "Portraits/fin.png"); 
+            ReplaceAsset<Texture2D>(e, "Characters/Sam", "Characters/fin.png");
+            ReplaceAsset<Texture2D>(e, "Portraits/Sam_Beach", "Portraits/fin_beach.png");
+            ReplaceAsset<Texture2D>(e, "Characters/Sam_Beach", "Characters/fin_beach.png");
+
             ReplaceAsset<Dictionary<string, string>>(e, "Data/NPCDispositions", "Data/NPCDispositions.json");
         }
 
-        private void ReplaceAsset<T>(AssetRequestedEventArgs e, string replacePath, string assetPath)
+        private void ReplaceAsset<T>(AssetRequestedEventArgs e, string replacePath, string assetPath)       
         {
             if (e.NameWithoutLocale.IsEquivalentTo(replacePath))
             {
