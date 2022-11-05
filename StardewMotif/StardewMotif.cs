e86b107fe574c17470291ded4e3341b46ef6d222
Vinyks <kevinbryks@online.de>
2022-11-05 01:12:02 +0100

    Added Sheep and Replaced More Dialogue

--- StardewMotif/StardewMotif.cs
+++ StardewMotif/StardewMotif.cs
@@ -52,24 +52,43 @@ namespace StardewMotif
             ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/Sam", "Characters/Dialogue/Fin.json");
             ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/MarriageDialogueSam", "Characters/Dialogue/MarriageDialogueFin.json");
 
+            //Misc Chars 
+            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/Sebastian", "Characters/Dialogue/Sebastian.json");
+            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/Pierre", "Characters/Dialogue/Pierre.json");
+
+
             ////General
             //Data
             ReplaceAsset<Dictionary<string, string>>(e, "Data/NPCDispositions", "Data/NPCDispositions.json");
             ReplaceAsset<Dictionary<int, string>>(e, "Data/SecretNotes", "Data/SecretNotes.json");
             ReplaceAsset<Dictionary<string, string>>(e, "Data/EngagementDialogue", "Data/EngagementDialogue.json");
             ReplaceAsset<Dictionary<int, string>>(e, "Data/weapons", "Data/weapons.json");
+            ReplaceAsset<Dictionary<int, string>>(e, "Data/Quests", "Data/Quests.json");
+            ReplaceAsset<Dictionary<int, string>>(e, "Data/ObjectInformation", "Data/ObjectInformation.json");
+            ReplaceAsset<Dictionary<string, string>>(e, "Data/mail", "Data/mail.json");
+            ReplaceAsset<Dictionary<string, string>>(e, "Data/FarmAnimals", "Data/FarmAnimals.json");
+
+            //Events
+            ReplaceAsset<Dictionary<string, string>>(e, "Data/Events/Beach", "Data/Events/Beach.json");
+            ReplaceAsset<Dictionary<string, string>>(e, "Data/Events/Town", "Data/Events/Town.json");
+            ReplaceAsset<Dictionary<string, string>>(e, "Data/Events/Forest", "Data/Events/Forest.json");
+            ReplaceAsset<Dictionary<string, string>>(e, "Data/Events/SamHouse", "Data/Events/SamHouse.json");
+            ReplaceAsset<Dictionary<string, string>>(e, "Data/Events/SebastianRoom", "Data/Events/SebastianRoom.json");
 
             //Character Dialogue
             ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/rainy", "Characters/Dialogue/rainy.json");
 
-            //Strings
+            //Strings 
             ReplaceAsset<Dictionary<string, string>>(e, "Strings/Characters", "Strings/Characters.json");
+            ReplaceAsset<Dictionary<string, string>>(e, "Strings/animationDescriptions", "Strings/animationDescriptions.json");
+            ReplaceAsset<Dictionary<string, string>>(e, "Strings/Locations", "Strings/Locations.json");
 
             //Loose Sprites
             ReplaceAsset<Texture2D>(e, "LooseSprites/logo", "LooseSprites/logo.png");
-            ReplaceAsset<Texture2D>(e, "LooseSprites/emojis", "LooseSprites/emojis.png");
+            ReplaceAsset<Texture2D>(e, "LooseSprites/emojis", "LooseSprites/emojis.png"); 
             ReplaceAsset<Texture2D>(e, "LooseSprites/Concessions", "LooseSprites/Concessions.png");
             ReplaceAsset<Texture2D>(e, "LooseSprites/Billboard", "LooseSprites/Billboard.png");
+            ReplaceAsset<Texture2D>(e, "LooseSprites/Cursors", "LooseSprites/Cursors.png");
 
             //MiniGames
             ReplaceAsset<Texture2D>(e, "MiniGames/TitleButtons", "MiniGames/TitleButtons.png");
@@ -78,6 +97,10 @@ namespace StardewMotif
             ReplaceAsset<Texture2D>(e, "Animals/BabySheep", "Animals/BabyWoo.png");
             ReplaceAsset<Texture2D>(e, "Animals/ShearedSheep", "Animals/ShearedWoo.png");
             ReplaceAsset<Texture2D>(e, "Animals/Sheep", "Animals/Woo.png");
+
+            //Maps
+            ReplaceAsset<Texture2D>(e, "Maps/springobjects", "Maps/SpringObjects.png");
+
         }
 
         private void ReplaceAsset<T>(AssetRequestedEventArgs e, string replacePath, string assetPath)       
