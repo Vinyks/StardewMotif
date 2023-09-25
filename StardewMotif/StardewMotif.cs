62590d68e6785dabfa0bacab6033f45ccb54f448
Vinyks <kevinbryks@online.de>
2023-09-25 03:56:38 +0200

    Reworked Everything
    -Changed all Dialogue
    -Added Jinja and Laphi
    -Added Kloteppich
    -Added English locale

--- StardewMotif/StardewMotif.cs
+++ StardewMotif/StardewMotif.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
-using StardewModdingAPI.Utilities;
 using StardewValley;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework.Graphics;
 using System.IO;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace StardewMotif
 {
     public class ModEntry : Mod
     {
-        public string CurrentLocale => throw new NotImplementedException();
+        public string CurrentLocale { get; set; }
+        public string LocalHeader { get; set; }
 
         public LocalizedContentManager.LanguageCode CurrentLocaleConstant => throw new NotImplementedException();
 
@@ -24,112 +24,202 @@ namespace StardewMotif
             helper.Events.Input.ButtonPressed += this.OnButtonPressed;
 
             helper.Events.Content.AssetRequested += this.OnAssetRequested;
+
+            helper.Events.Content.LocaleChanged += this.OnLocaleChange;
         }
+
+        public void OnLocaleChange(object sender, LocaleChangedEventArgs e)
+        {
+            CurrentLocale = e.NewLocale;
+
+            LocalHeader = CurrentLocale == "" ? "" : "." + CurrentLocale;
+            //this.Monitor.Log($"Current Locale Header :" + LocalHeader, LogLevel.Debug);
+        }
+
+        //Json Names
+        readonly string[] stringStringData = { "CookingRecipes", "EngagementDialogue", "ExtraDialogue",
+            "FarmAnimals", "mail", "NPCDispositions", "NPCGiftTastes" };
+
+        readonly string[] stringObjectData = { "ConcessionTastes"};
+
+        readonly string[] intStringData = { "Furniture", "SecretNotes", "weapons", "Quests", "ObjectInformation" };
+
+        readonly string[] events = { "Beach", "BusStop", "Farm", "FarmHouse", "Forest", "HaleyHouse", "SamHouse", "SienceHouse", "SebastianRoom", "Town" };
+
+        readonly string[] festivals = { "spring13" };
+
+        readonly string[] strings = { "Characters", "animationDescriptions", "Locations", "SpecialOrderStrings", "StringsFromCSFiles", "StringsFromMaps" };
+        readonly string[] stringSchedules = { "Maru", "Sebastian" };
+
+        readonly string[] dialogue = {"Leah","Abigail","Caroline","Demetrius","Jas","Jodi","Kent","LeoMainland","Lewis","MarriageDialogue",
+            "MarriageDialogueAbigail","MarriageDialogueMaru","MarriageDialogueSam","MarriageDialogueSebastian","Pam","Penny","Pierre", "rainy","Robin","Sam","Sebastian","Vincent" };
+
+        //Sprite Names
+        readonly string[] looseSpritesPaths = { "logo", "emojis", "Concessions" };
+        readonly string[] looseSpritesPathsLocalized = { "Billboard", "Cursors" };
+
+        readonly string[] animals = { "BabySheep", "ShearedSheep", "Sheep" };
+
+
         public void OnAssetRequested(object sender, AssetRequestedEventArgs e)
         {
             ////Characters
             //Luny
-            ReplaceAsset<Texture2D>(e, "Portraits/Abigail", "Portraits/luny.png"); 
-            ReplaceAsset<Texture2D>(e, "Characters/Abigail", "Characters/luny.png");
-            ReplaceAsset<Texture2D>(e, "Portraits/Abigail_Beach", "Portraits/luny_beach.png");
-            ReplaceAsset<Texture2D>(e, "Characters/Abigail_Beach", "Characters/luny_beach.png");
-            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/Abigail", "Characters/Dialogue/Luny.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/MarriageDialogueAbigail", "Characters/Dialogue/MarriageDialogueLuny.json");
+            ReplaceCharacter(e, "Abigail", "Luny");
 
             //Mona
-            ReplaceAsset<Texture2D>(e, "Portraits/Leah", "Portraits/mona.png");
-            ReplaceAsset<Texture2D>(e, "Characters/Leah", "Characters/mona.png");
-            ReplaceAsset<Texture2D>(e, "Portraits/Leah_Beach", "Portraits/mona_beach.png");
-            ReplaceAsset<Texture2D>(e, "Characters/Leah_Beach", "Characters/mona_beach.png");
-            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/Leah", "Characters/Dialogue/Mona.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/MarriageDialogueLeah", "Characters/Dialogue/MarriageDialogueMona.json");
+            ReplaceCharacter(e, "Leah", "Mona");
 
             //Fin
-            ReplaceAsset<Texture2D>(e, "Portraits/Sam", "Portraits/fin.png"); 
-            ReplaceAsset<Texture2D>(e, "Characters/Sam", "Characters/fin.png");
-            ReplaceAsset<Texture2D>(e, "Portraits/Sam_Beach", "Portraits/fin_beach.png");
-            ReplaceAsset<Texture2D>(e, "Characters/Sam_Beach", "Characters/fin_beach.png");
-            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/Sam", "Characters/Dialogue/Fin.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/MarriageDialogueSam", "Characters/Dialogue/MarriageDialogueFin.json");
+            ReplaceCharacter(e, "Sam", "Fin");
 
             //Jinja
-            ReplaceAsset<Texture2D>(e, "Portraits/Robin", "Portraits/jinja.png");
-            ReplaceAsset<Texture2D>(e, "Characters/Robin", "Characters/jinja.png");
-            ReplaceAsset<Texture2D>(e, "Portraits/Robin_Beach", "Portraits/jinja_beach.png");
-            ReplaceAsset<Texture2D>(e, "Characters/Robin_Beach", "Characters/jinja_beach.png");
+            ReplaceCharacter(e, "Robin", "Jinja");
 
             //Laphi
-            ReplaceAsset<Texture2D>(e, "Portraits/Penny", "Portraits/laphi.png");
-            ReplaceAsset<Texture2D>(e, "Characters/Penny", "Characters/laphi.png");
-            ReplaceAsset<Texture2D>(e, "Portraits/Penny_Beach", "Portraits/laphi_beach.png");
-            ReplaceAsset<Texture2D>(e, "Characters/Penny_Beach", "Characters/laphi_beach.png");
+            ReplaceCharacter(e, "Penny", "Laphi");
 
             //Misc Chars 
-            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/Sebastian", "Characters/Dialogue/Sebastian.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/Pierre", "Characters/Dialogue/Pierre.json");
-
-
+            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/Sebastian", "Characters/Dialogue/Sebastian", "json");
+            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/Pierre", "Characters/Dialogue/Pierre", "json");
 
             ////General
             //Data
-            ReplaceAsset<Dictionary<string, string>>(e, "Data/NPCDispositions", "Data/NPCDispositions.json");
-            ReplaceAsset<Dictionary<int, string>>(e, "Data/SecretNotes", "Data/SecretNotes.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Data/EngagementDialogue", "Data/EngagementDialogue.json");
-            ReplaceAsset<Dictionary<int, string>>(e, "Data/weapons", "Data/weapons.json");
-            ReplaceAsset<Dictionary<int, string>>(e, "Data/Quests", "Data/Quests.json");
-            ReplaceAsset<Dictionary<int, string>>(e, "Data/ObjectInformation", "Data/ObjectInformation.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Data/mail", "Data/mail.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Data/FarmAnimals", "Data/FarmAnimals.json");
+            ReplaceAsset<List<object>>(e, "Data/ConcessionTastes", "Data/ConcessionTastes", "json", false);
+
+            ReplaceAssets<Dictionary<string, string>>(e, "Data/", stringStringData, "json");
+
+            ReplaceAsset<List<StardewValley.GameData.Movies.MovieCharacterReaction>>(e, "Data/MoviesReactions", "Data/MoviesReactions", "json");
+
+            ReplaceAssets<Dictionary<int, string>>(e, "Data/", intStringData, "json");
+
+            //Festivals
+            ReplaceAssets<Dictionary<string, string>>(e, "Data/Festivals/", festivals, "json");
 
             //Events
-            ReplaceAsset<Dictionary<string, string>>(e, "Data/Events/Beach", "Data/Events/Beach.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Data/Events/Town", "Data/Events/Town.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Data/Events/Forest", "Data/Events/Forest.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Data/Events/SamHouse", "Data/Events/SamHouse.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Data/Events/SebastianRoom", "Data/Events/SebastianRoom.json");
+            ReplaceAssets<Dictionary<string, string>>(e, "Data/Events/", events, "json");
 
             //Character Dialogue
-            ReplaceAsset<Dictionary<string, string>>(e, "Characters/Dialogue/rainy", "Characters/Dialogue/rainy.json");
+            ReplaceAssets<Dictionary<string, string>>(e, "Characters/Dialogue/", dialogue, "json");
 
             //Strings 
-            ReplaceAsset<Dictionary<string, string>>(e, "Strings/Characters", "Strings/Characters.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Strings/animationDescriptions", "Strings/animationDescriptions.json");
-            ReplaceAsset<Dictionary<string, string>>(e, "Strings/Locations", "Strings/Locations.json");
+            ReplaceAssets<Dictionary<string, string>>(e, "Strings/", strings, "json");
+
+            //String Schedules
+            ReplaceAssets<Dictionary<string, string>>(e, "Strings/schedules/", stringSchedules, "json");
 
             //Loose Sprites
-            ReplaceAsset<Texture2D>(e, "LooseSprites/logo", "LooseSprites/logo.png");
-            ReplaceAsset<Texture2D>(e, "LooseSprites/emojis", "LooseSprites/emojis.png"); 
-            ReplaceAsset<Texture2D>(e, "LooseSprites/Concessions", "LooseSprites/Concessions.png");
-            ReplaceAsset<Texture2D>(e, "LooseSprites/Billboard", "LooseSprites/Billboard.png");
-            ReplaceAsset<Texture2D>(e, "LooseSprites/Cursors", "LooseSprites/Cursors.png");
+            ReplaceAssets<Texture2D>(e, "LooseSprites/", looseSpritesPaths, "png", false);
+            ReplaceAssets<Texture2D>(e, "LooseSprites/", looseSpritesPathsLocalized, "png");
 
             //MiniGames
-            ReplaceAsset<Texture2D>(e, "MiniGames/TitleButtons", "MiniGames/TitleButtons.png");
+            ReplaceAsset<Texture2D>(e, "MiniGames/TitleButtons", "MiniGames/TitleButtons", "png");
 
             //Animals
-            ReplaceAsset<Texture2D>(e, "Animals/BabySheep", "Animals/BabyWoo.png");
-            ReplaceAsset<Texture2D>(e, "Animals/ShearedSheep", "Animals/ShearedWoo.png");
-            ReplaceAsset<Texture2D>(e, "Animals/Sheep", "Animals/Woo.png");
+            ReplaceAssets<Texture2D>(e, "Animals/", animals, "png", false);
 
             //Maps
-            ReplaceAsset<Texture2D>(e, "Maps/springobjects", "Maps/SpringObjects.png");
+            ReplaceAsset<Texture2D>(e, "Maps/springobjects", "Maps/SpringObjects", "png", false);
 
+            ReplaceAsset<Texture2D>(e, "TileSheets/furniture", "TileSheets/furniture", "png", false);
         }
 
-        private void ReplaceAsset<T>(AssetRequestedEventArgs e, string replacePath, string assetPath)       
+        private void ReplaceAssets<T>(AssetRequestedEventArgs e, string commonPath, string[] names, string type, bool localize = true)
         {
-            if (e.NameWithoutLocale.IsEquivalentTo(replacePath))
+            foreach (string path in names)
             {
-                e.LoadFromModFile<T>("assets/"+assetPath, AssetLoadPriority.Medium);
+                ReplaceAsset<T>(e, commonPath + path, commonPath + path, type, localize);
             }
         }
 
+        private void ReplaceAsset<T>(AssetRequestedEventArgs e, string replacePath, string assetPath, string type, bool localize = true)
+        {
+            //Check if it is the Asset we are looking for
+            if (e.NameWithoutLocale.IsEquivalentTo(replacePath))
+            {
+                //Localized Asset
+                //Monitor.Log($"Without: " + e.NameWithoutLocale + " With: " + e.Name, LogLevel.Debug);
+                if (localize)
+                {
+                    e.LoadFromModFile<T>("assets/" + assetPath + LocalHeader + "." + type, AssetLoadPriority.Medium);
+                }
+                //Unlocalized Asset
+                else
+                {
+                    e.LoadFromModFile<T>("assets/" + assetPath + "." + type, AssetLoadPriority.Medium);
+                }
+            }
+        }
+
+        private void ReplaceCharacter(AssetRequestedEventArgs e, string replaceChar, string newChar)
+        {
+            ReplaceAsset<Texture2D>(e, "Portraits/" + replaceChar, "Portraits/" + newChar, "png", false);
+            ReplaceAsset<Texture2D>(e, "Characters/" + replaceChar, "Characters/" + newChar, "png", false);
+            ReplaceAsset<Texture2D>(e, "Portraits/" + replaceChar + "_Beach", "Portraits/" + newChar + "_Beach", "png", false);
+            ReplaceAsset<Texture2D>(e, "Characters/" + replaceChar + "_Beach", "Characters/" + newChar + "_Beach", "png", false);
+        }
+
+        string[] debugDialogueNPCs = {"Abigail", "Sam","Leah","Robin","Penny" };
+
+        Task debugTask;
         private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
         {
             if (!Context.IsWorldReady)
                 return;
+            /*
+            if (e.Button.ToString()[0] == 'K')
+            {
+                if((debugTask != null && debugTask.IsCompleted) || debugTask == null)
+                {
+                    debugTask = DebugDialogue("Robin");
+                    this.Monitor.Log("Activated Debug", LogLevel.Debug);
+                }
+            }
+            */
+        }
+        bool queueNextDialogue = false;
+        public async Task DebugDialogue(string npcName)
+        {
+            this.Monitor.Log("Called", LogLevel.Debug);
 
-            //this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+            string fullPath = @"\Steam\steamapps\common\Stardew Valley\Mods\StardewMotif\assets\Characters\Dialogue\";
+            string dialoguePath = @"\Characters\Dialogue\";
+            dialoguePath += npcName;
+
+            this.Monitor.Log("Dialogue Path: " + dialoguePath, LogLevel.Debug);
+            NPC npc = Utility.fuzzyCharacterSearch(npcName, true);
+            string json;
+            try
+            {
+                json = File.ReadAllText(fullPath+ npcName + ".json");
+                Dictionary<string, string> data = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+                foreach (string key in data.Keys)
+                {
+                    string dialogue = Game1.content.LoadStringReturnNullIfNotFound(dialoguePath + ":" + key);
+                   
+                    if(dialogue != null)
+                    {
+                        string[] split = dialogue.Split(new[] { "#$e#" }, StringSplitOptions.RemoveEmptyEntries);
+                        
+                        foreach (string s in split)
+                        {
+                            this.Monitor.Log("Showing dialogue: " + s, LogLevel.Debug);
+                            Game1.drawDialogue(npc, s);
+                            while (Game1.currentSpeaker != null)
+                            {
+                                await Task.Yield();
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                this.Monitor.Log("Except: " + e, LogLevel.Debug);
+            }
+
+            this.Monitor.Log("Dialogue Path: " + dialoguePath, LogLevel.Debug);
+            
         }
     }
 }
